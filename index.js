64a194f4078c39d8aaadffe9ce13b5abdefdcfd2
konradtbudny <konradtbudny@gmail.com>
2022-03-28 12:52:11 -0500

    set up users component in api

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const PORT = 3000;
 const express = require('express');
+const morgan = require('morgan');
+const apiRouter = require('./api');
+const { client } = require('./db');
+const PORT = 3000;
 const server = express();
+
+client.connect();
 
 server.listen(PORT, () => {
   console.log('The server is up on port', PORT)
 });
+
+server.use('/api', apiRouter);
+server.use(morgan('dev'));
+server.use(express.json())
+
 server.use((req, res, next) => {
     console.log("<____Body Logger START____>");
     console.log(req.body);
@@ -12,4 +22,3 @@ server.use((req, res, next) => {
   
     next();
   });
-  
