e3a49ad42be7bc75ee461f106e3f86d8f5acea86
konradtbudny <konradtbudny@gmail.com>
2022-03-28 12:38:36 -0500

    Seeting up part 2

---
+++ index.js
@@ -0,0 +1,15 @@
+const PORT = 3000;
+const express = require('express');
+const server = express();
+
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
+server.use((req, res, next) => {
+    console.log("<____Body Logger START____>");
+    console.log(req.body);
+    console.log("<_____Body Logger END_____>");
+  
+    next();
+  });
+  
