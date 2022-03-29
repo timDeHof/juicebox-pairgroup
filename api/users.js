61139c517e689545d546c923726c0d5a058ad37e
timDeHof <this.is.tim.dehof@gmail.com>
2022-03-29 11:44:24 -0400

    fixed middleware location

--- api/users.js
+++ api/users.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const {getAllUsers,createUser}=require('../db');
+const express = require("express");
+const { getAllUsers, createUser, getUserByUsername } = require("../db");
 const usersRouter = express.Router();
 
 usersRouter.use((req, res, next) => {
@@ -7,14 +7,14 @@ usersRouter.use((req, res, next) => {
 
   next();
 });
-usersRouter.post('/login', async (req, res, next) => {
+usersRouter.post("/login", async (req, res, next) => {
   const { username, password } = req.body;
 
   // request must have both
   if (!username || !password) {
     next({
       name: "MissingCredentialsError",
-      message: "Please supply both a username and password"
+      message: "Please supply both a username and password",
     });
   }
 
@@ -25,18 +25,18 @@ usersRouter.post('/login', async (req, res, next) => {
       // create token & return to user
       res.send({ message: "you're logged in!" });
     } else {
-      next({ 
-        name: 'IncorrectCredentialsError', 
-        message: 'Username or password is incorrect'
+      next({
+        name: "IncorrectCredentialsError",
+        message: "Username or password is incorrect",
       });
     }
-  } catch(error) {
+  } catch (error) {
     console.log(error);
     next(error);
   }
 });
 
-usersRouter.post('/register', async (req, res, next) => {
+usersRouter.post("/register", async (req, res, next) => {
   console.log(req.body);
   const { username, password, name, location } = req.body;
 
@@ -45,8 +45,8 @@ usersRouter.post('/register', async (req, res, next) => {
 
     if (_user) {
       next({
-        name: 'UserExistsError',
-        message: 'A user by that username already exists'
+        name: "UserExistsError",
+        message: "A user by that username already exists",
       });
     }
 
@@ -57,26 +57,30 @@ usersRouter.post('/register', async (req, res, next) => {
       location,
     });
 
-    const token = jwt.sign({ 
-      id: user.id, 
-      username
-    }, process.env.JWT_SECRET, {
-      expiresIn: '1w'
-    });
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1w",
+      }
+    );
 
-    res.send({ 
+    res.send({
       message: "thank you for signing up",
-      token 
+      token,
     });
   } catch ({ name, message }) {
-    next({ name, message })
-  } 
+    next({ name, message });
+  }
 });
 
-usersRouter.get('/', async (req, res) => {
-    const users=await getAllUsers();
-    res.send({
-    users
+usersRouter.get("/", async (req, res) => {
+  const users = await getAllUsers();
+  res.send({
+    users,
   });
 });
 
