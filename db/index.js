475c1cf69f2574943e4cd6c34b4cc451925be27d
konradtbudny <konradtbudny@gmail.com>
2022-03-30 12:56:12 -0500

    another heroku fix

--- db/index.js
+++ db/index.js
@@ -1,7 +1,10 @@
 const {Client} = require("pg");
 const {rows} = require("pg/lib/defaults");
 
-const client = new Client(process.env.DATABASE_URL||"postgres://localhost:5432/juicebox-dev");
+const client = new Client({
+  connectionString: process.env.DATABASE_URL||"postgres://localhost:5432/juicebox-dev",
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
 
 async function getAllUsers() {
     const {rows} = await client.query(`SELECT id, username, name, location, active
