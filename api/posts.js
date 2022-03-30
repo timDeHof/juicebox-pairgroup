308f04e976e949bc6c27038314c23a96e4f1d81a
konradtbudny <konradtbudny@gmail.com>
2022-03-30 11:14:55 -0500

    deleting posts by id and filter inactive posts for unauthorized users

--- api/posts.js
+++ api/posts.js
@@ -11,10 +11,18 @@ postsRouter.use((req, res, next) => {
 });
 
 postsRouter.get("/", async (req, res) => {
-  const posts = await getAllPosts();
-  res.send({
-    posts,
-  });
+  try{
+    const allPosts = await getAllPosts();
+
+    const posts=allPosts.filter(post=>{
+      return post.active ||(req.user &&post.author.id==req.user.id);
+    });
+    res.send({
+      posts,
+    });
+  }catch({name, message}){
+    next({name,message});
+  }
 });
 
 postsRouter.post("/", requireUser, async (req, res, next) => {
@@ -82,5 +90,30 @@ postsRouter.patch("/:postId", requireUser, async (req, res, next) => {
     next({ name, message });
   }
 });
+
+postsRouter.delete('/:postId', requireUser, async (req, res, next) => {
+  try {
+    const post = await getPostById(req.params.postId);
+
+    if (post && post.author.id === req.user.id) {
+      const updatedPost = await updatePost(post.id, { active: false });
+
+      res.send({ post: updatedPost });
+    } else {
+      // if there was a post, throw UnauthorizedUserError, otherwise throw PostNotFoundError
+      next(post ? { 
+        name: "UnauthorizedUserError",
+        message: "You cannot delete a post which is not yours"
+      } : {
+        name: "PostNotFoundError",
+        message: "That post does not exist"
+      });
+    }
+
+  } catch ({ name, message }) {
+    next({ name, message })
+  }
+});
+
 
 module.exports = postsRouter;
--- db/index.js
+++ db/index.js
@@ -270,6 +270,14 @@ async function getPostById(postId) {
     `,
       [postId]
     );
+
+    if (!post) {
+      throw {
+        name: "PostNotFoundError",
+        message: "Could not find a post with that postId"
+      };
+    }
+
     const { rows: tags } = await client.query(
       `
     SELECT tags.*
